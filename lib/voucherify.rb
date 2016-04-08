a83a62a3325ffc974a53eda870da596a7efd2261
pawelrychlik <pawel.rychlik@rspective.pl>
2016-04-08 22:58:51 +0200

    get voucher

--- lib/voucherify.rb
+++ lib/voucherify.rb
@@ -1,5 +1,36 @@
 require "voucherify/version"
+require "net/http"
+require "uri"
+require "json"
 
-module Voucherify
-  # Your code goes here...
+require "pry"
+
+
+class Voucherify
+  def initialize(options)
+    @backendUrl = "https://api.voucherify.io/v1"
+    @options = options
+  end
+
+  def get(code)
+    uri = URI.parse("#{@backendUrl}/vouchers/#{URI.encode(code)}")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request["X-App-Id"] = @options["applicationId"]
+    request["X-App-Token"] = @options["clientSecretKey"]
+    request["X-Voucherify-Channel"] = "Ruby-SDK"
+    request["Accept"] = "application/json"
+
+    response = http.request(request)
+    # puts response.code             # => 301
+    # puts response.body             # => The body (HTML, XML, blob, whatever)
+
+    # binding.pry
+    JSON.parse(response.body)
+  end
+
 end
--- lib/voucherify/version.rb
+++ lib/voucherify/version.rb
@@ -1,3 +1,3 @@
-module Voucherify
+class Voucherify
   VERSION = "0.1.0"
 end
