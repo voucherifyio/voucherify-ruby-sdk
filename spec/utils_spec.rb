63181d024efbe8793be81b0383971b8461bc4a75
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2016-07-05 10:02:34 +0200

    Update tests to work with Utils module

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -1,43 +1,35 @@
 require 'voucherify/utils'
 
+include Utils;
+
 describe Utils do
     it 'rounds money' do
-        utils = Utils.new;
-
         amount = 1234.5678;
 
-        rounded_money = utils.round_money(amount)
+        rounded_money = Utils.round_money(amount)
 
         expect(rounded_money).to eq 1234.57
     end
 
     it 'validates percent discount' do
-        utils = Utils.new;
-
-        expect{utils.validate_percent_discount(1000)}.to raise_error("Invalid voucher, percent discount should be between 0-100.")
-        expect{utils.validate_percent_discount(-1)}.to raise_error("Invalid voucher, percent discount should be between 0-100.")
-        expect{utils.validate_percent_discount('string')}.to raise_error("Invalid voucher, percent discount should be between 0-100.")
+        expect{Utils.validate_percent_discount(1000)}.to raise_error("Invalid voucher, percent discount should be between 0-100.")
+        expect{Utils.validate_percent_discount(-1)}.to raise_error("Invalid voucher, percent discount should be between 0-100.")
+        expect{Utils.validate_percent_discount('string')}.to raise_error("Invalid voucher, percent discount should be between 0-100.")
     end
 
     it 'validates amount discount' do
-        utils = Utils.new;
-
-        expect{utils.validate_amount_discount(-1)}.to raise_error("Invalid voucher, amount discount must be bigger than zero.")
-        expect{utils.validate_amount_discount('string')}.to raise_error("Invalid voucher, amount discount must be bigger than zero.")
+        expect{Utils.validate_amount_discount(-1)}.to raise_error("Invalid voucher, amount discount must be bigger than zero.")
+        expect{Utils.validate_amount_discount('string')}.to raise_error("Invalid voucher, amount discount must be bigger than zero.")
 
     end
 
     it 'validates unit discount' do
-        utils = Utils.new;
-
-        expect{utils.validate_unit_discount(-1)}.to raise_error("Invalid voucher, unit discount must be bigger than zero.")
-        expect{utils.validate_unit_discount('string')}.to raise_error("Invalid voucher, unit discount must be bigger than zero.")
+        expect{Utils.validate_unit_discount(-1)}.to raise_error("Invalid voucher, unit discount must be bigger than zero.")
+        expect{Utils.validate_unit_discount('string')}.to raise_error("Invalid voucher, unit discount must be bigger than zero.")
 
     end
 
     it 'calculates price with percent voucher' do
-        utils = Utils.new;
-
         percent_voucher_object = {
             discount: {
                 percent_off: 10.0,
@@ -45,12 +37,10 @@ describe Utils do
             }
         }
 
-        expect(utils.calculate_price(20, percent_voucher_object, 0)).to eq 18
+        expect(Utils.calculate_price(20, percent_voucher_object, 0)).to eq 18
     end
 
     it 'calculates price with amount voucher' do
-        utils = Utils.new;
-
         amount_voucher_object = {
             discount: {
                 amount_off: 10.0,
@@ -58,12 +48,10 @@ describe Utils do
             }
         }
 
-        expect(utils.calculate_price(20, amount_voucher_object, 0)).to eq 10
+        expect(Utils.calculate_price(20, amount_voucher_object, 0)).to eq 10
     end
 
     it 'calculates price with unit voucher' do
-        utils = Utils.new;
-
         unit_voucher_object = {
             discount: {
                 unit_off: 2.0,
@@ -71,24 +59,20 @@ describe Utils do
             }
         }
 
-        expect(utils.calculate_price(20, unit_voucher_object, 8)).to eq 4
+        expect(Utils.calculate_price(20, unit_voucher_object, 8)).to eq 4
     end
 
     it 'calculates price with unknown voucher' do
-        utils = Utils.new;
-
         void_voucher_object = {
             discount: {
                 type: 'VOID'
             }
         }
 
-        expect{utils.calculate_price(20, void_voucher_object, 8)}.to raise_error("Unsupported discount type")
+        expect{Utils.calculate_price(20, void_voucher_object, 8)}.to raise_error("Unsupported discount type")
     end
     
     it 'calculates discount with percent voucher' do
-        utils = Utils.new;
-
         percent_voucher_object = {
             discount: {
                 percent_off: 10.0,
@@ -96,12 +80,10 @@ describe Utils do
             }
         }
 
-        expect(utils.calculate_discount(20, percent_voucher_object, 0)).to eq 2
+        expect(Utils.calculate_discount(20, percent_voucher_object, 0)).to eq 2
     end
 
     it 'calculates discount with amount voucher' do
-        utils = Utils.new;
-
         amount_voucher_object = {
             discount: {
                 amount_off: 7.0,
@@ -109,12 +91,10 @@ describe Utils do
             }
         }
 
-        expect(utils.calculate_discount(20, amount_voucher_object, 0)).to eq 7
+        expect(Utils.calculate_discount(20, amount_voucher_object, 0)).to eq 7
     end
 
     it 'calculates discount with unit voucher' do
-        utils = Utils.new;
-
         unit_voucher_object = {
             discount: {
                 unit_off: 2.0,
@@ -122,18 +102,16 @@ describe Utils do
             }
         }
 
-        expect(utils.calculate_discount(20, unit_voucher_object, 8)).to eq 16
+        expect(Utils.calculate_discount(20, unit_voucher_object, 8)).to eq 16
     end
 
     it 'calculates discount with unknown voucher' do
-        utils = Utils.new;
-
         void_voucher_object = {
             discount: {
                 type: 'VOID'
             }
         }
 
-        expect{utils.calculate_discount(20, void_voucher_object, 8)}.to raise_error("Unsupported discount type")
+        expect{Utils.calculate_discount(20, void_voucher_object, 8)}.to raise_error("Unsupported discount type")
     end
 end
