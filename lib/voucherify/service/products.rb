2d293e4bd50b5ef6da45255d631d4650e84ff24c
Łukasz Kulig <lukasz.kulig88@gmail.com>
2017-01-02 17:49:41 +0100

    Fixed tests

--- lib/voucherify/service/products.rb
+++ lib/voucherify/service/products.rb
@@ -18,7 +18,7 @@ module Voucherify
       end
 
       def update(product)
-        @client.put("/products/#{URI.encode(product['product_id'] || product[:product_id])}", product.to_json)
+        @client.put("/products/#{URI.encode(product['id'] || product[:id])}", product.to_json)
       end
 
       def delete(product_id)
@@ -30,23 +30,23 @@ module Voucherify
       end
 
       def create_sku(product_id, sku)
-        @client.post("/products/#{URI.encode(product_id)}/skus", sku)
+        @client.post("/products/#{URI.encode(product_id)}/skus", sku.to_json)
       end
 
       def get_sku(product_id, sku_id)
-        @client.get("/products/#{URL.encode(product_id)}/skus/#{URL.encode(sku_id)}")
+        @client.get("/products/#{URI.encode(product_id)}/skus/#{URI.encode(sku_id)}")
       end
 
       def update_sku(product_id, sku)
-        @client.put("/products/#{URL.encode(product_id)}/skus/#{URL.encode(sku['id'] || sku[:id])}")
+        @client.put("/products/#{URI.encode(product_id)}/skus/#{URI.encode(sku['id'] || sku[:id])}", sku.to_json)
       end
 
       def delete_sku(product_id, sku_id)
-        @client.delete("/products/#{URL.encode(product_id)}/skus/#{URL.encode(sku_id)}")
+        @client.delete("/products/#{URI.encode(product_id)}/skus/#{URI.encode(sku_id)}")
       end
 
       def list_skus(product_id)
-        @client.get("/products/#{URL.encode(product_id)}/skus")
+        @client.get("/products/#{URI.encode(product_id)}/skus")
       end
     end
   end
