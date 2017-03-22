6138ac88bc0d3a96862dec77ddca89c6d52e6de1
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2017-03-22 10:57:06 +0100

    updated example

--- examples/vouchers.rb
+++ examples/vouchers.rb
@@ -4,13 +4,18 @@ voucherify = Voucherify::Client.new({
                                         :applicationId => 'c70a6f00-cf91-4756-9df5-47628850002b',
                                         :clientSecretKey => '3266b9f8-e246-4f79-bdf0-833929b1380c'
                                     })
-
-puts voucherify.vouchers.validate('91Ft4U', {
+begin
+    puts voucherify.validations.validate_voucher('91Ft4U', {
     tracking_id: 'john@lemon.com',
     order: {
-        amount: 1000
+      amount: 1000
     }
-})
+  })
+rescue Voucherify::VoucherifyError => e
+    puts e.message
+    puts e.details
+    puts e.key
+end
 
 query = {limit: 10, skip: 20, category: 'API Test'}
 puts voucherify.vouchers.list(query)
